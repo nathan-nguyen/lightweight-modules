50960fef28596ac9afde5e0fc4eb9fdc5fcb153a
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-08-07 23:01:40 +0800

    [Data-Extractor] Fixed syntax issue

--- data-extractor/SelectQuery.java
+++ data-extractor/SelectQuery.java
@@ -2,7 +2,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.jdbc.dao.DataAccessException;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
 
